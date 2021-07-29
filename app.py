ddb2cf957eb15e8230643af182f22c7df0c9436d
YogeshMaan <170030012@iitdh.ac.in>
2021-07-29 12:18:40 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import cv2
+import pose_estimation_class as pm
+import mediapipe as mp
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/after', methods = ['GET', 'POST'])
+def after():
+    try:
+        img = request.files['file1']
+        img.save('static/file.jpg')
+
+        #======================================
+        img1 = cv2.imread('static/file.jpg')
+
+        # Need Image as "img"
+        detector = pm.PoseDetector()
+        # img1 = cv2.resize(img1, (416, 416))
+
+        img1, p_landmarks, p_connections = detector.findPose(img1, False)
+
+        mp.solutions.drawing_utils.draw_landmarks(img1, p_landmarks, p_connections)
+
+        # We have img to show
+        cv2.imwrite('static/after.jpg', img1)
+
+        return render_template('estimated.html')
+    except:
+        return  render_template('error.html')
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', port=5000, debug=True)
+
+
+
+
+
+
+
